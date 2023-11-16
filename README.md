779260ced34928eacab5aff3afdc4275831848e5
Ayoub CHAABAT <chaabat.02@gmail.com>
2023-11-16 19:15:30 +0100

    CHbank

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Gestion_bancaire
+gestion de banque
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400;500;600;700&family=Kdam+Thmor+Pro&display=swap" rel="stylesheet">
